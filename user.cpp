c881ec0da69929d4dd70a627c33197b4851c5b7c
pmortensen0 <pmortensen0916@nevada.unr.edu>
2021-04-29 10:50:39 -0700

    more updated layout to user.cpp

--- user.cpp
+++ user.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "user.h"
+#include "wav.h"
+
+void User::title(){ //prints out list of user actions (w/ #ed responses)
+
+} 
+
+std::string User::prompt(){ //prompts user and reads in response
+
+} 
+
+void User::getSongs(std::vector<Wav*> songs){ //prints out list of user songs
+
+} 
+
+//editMetadata
+//When the modification is complete, the file must be saved with the new metadata
+//The modifications should override any current metadata, or create the metadata if it didn’t exist previously
+void User::editMetadata(std::vector<Wav*> songs){ //allows user to edit the metadata of song
+
+} 
+
 //process
 //check for bit depth if(wav.bit_depth == 8){}else if(wav.bitdepth == 16){}
 //The user may choose to process a file by choosing one or more processors
@@ -7,8 +33,10 @@
 //After processing, the user should be prompted for a file name and if valid, 
     //a new file is created with the processed audio
     //The user is not allowed to save the file under the same name as any of the existing files
+void User::process(int process){ //gets what process the user wants to do and completes process (via a call to wav)
 
+} 
 
-//editMetadata
-//When the modification is complete, the file must be saved with the new metadata
-//The modifications should override any current metadata, or create the metadata if it didn’t exist previously
+void User::save(){ //export a CSV file
+
+} 
