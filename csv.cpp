64b75795c191ae99e719d769cb070febc3bdd5be
azhou-coder <azhou@nevada.unr.edu>
2021-05-01 17:43:16 -0700

    Added stuff-Need help with accessing info chunk

--- csv.cpp
+++ csv.cpp
@@ -3,35 +3,23 @@
 #include <iostream>
 #include "csv.h"
 
-//Dont know where to start with letting the user export the CSV file -Annie
+//I do not where to start to access info chunk for editing - Annie
 
-void CSV::readFile(const std::string &filename){
-	std::string line;
-	//do I need line below? 
-	//paige: this has been taken care of in driver. ideally, we don't want to pass files anywhere 
-	//unless they have already been checked. therefore, we will only pass legit files into any 
-	//classes and no further checks are neeeded :)
+/*
+CSV::CSV(std::string data){
+	
+}
+*/
 
-	//paige: also, you might want to ask sarah, but I don't think that the csv class need a read file. 
-	//the metadata should already be stored in the attributes of the wav class/object for each song.
-	//I think csv only need a write file to handle user edits (?)
-	std::ifstream file(filename, std::ios::binary | std::ios:in);
-	if(file.is_open()){
-		while(getline (file, line)){
-			std::cout << line << "\n";
-		}
-	file.close();
-	}
-	else{
-		std::cout << "Cannot open CSV file";	
-	}
-	return 0;
+unsigned char *Wav::getBuffer(){
+	return buffer;
 }
 
 void CSV::writeFile(const std::string &outFileName){
 	//do I need line below?
 	std::ofstream outFile(outFileName, std::ios::out | std::ios::binary);
 	if(file.is_open()){
+		outFile.write(); //? Should I put this here?
 		file << "user edits metadata?\n";
 		outFile.close();
 	}else{
@@ -40,3 +28,11 @@ void CSV::writeFile(const std::string &outFileName){
 	//outFile.write();
 	//outFile.close();
 }
+
+CSV::~CSV(){
+	if(buffer != NULL){
+		delete[] buffer;
+	}
+}
+
+//void CSV::setData();
--- csv.h
+++ csv.h
@@ -2,15 +2,19 @@
 #define CSV_H
 
 #include "GroupProj_CS202 - Sheet1.csv"
+#include "wav.h"
 
 class CSV{
 	unsigned char* buffer = NULL;
+	std::string data;
 public:
-	void readFile(const std::string &filename);
+	CSV(std::string data);
 	void writeFile(const std::string &outFileName);
 
 	unsigned char* getBuffer();
 	int getBuffersize() const;
+	std::string getData() const;
+	void setData() const;
 
 	virtual ~CSV();
 
