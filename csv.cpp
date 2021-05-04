89ca408fd80c546dcf48b67fe5e0ec93cc659c63
azhou-coder <azhou@nevada.unr.edu>
2021-05-04 00:08:25 -0700

    added stuff and commentary questions

--- csv.cpp
+++ csv.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include "csv.h"
 
-//I do not where to start to access info chunk for editing - Annie
 //Paige: the metadata should be stored in the wave_header in tilte and artist on lines 45 and 46
 //I should have read this in, but I can't test to see if it works, the makefile was gin=ving me a weird error
 //you shoould just be able to incule and inherit this file and it will allow you access to all the 
@@ -11,7 +10,7 @@
 //you can them directly edit from here
 
 /*
-CSV::CSV(std::string data){
+CSV::CSV(std::string title, std::string artist){ 
 	
 }
 */
@@ -21,9 +20,12 @@ CSV::CSV(std::string data){
 //stuff in from the vector of songs (or the vector itself) and call either 
 //wav.get8bitBuffer() or wav.get16bitBuffer() depending on the bit depth of the song 
 //(also found under wave_header line 37)
+/*
 unsigned char *Wav::getBuffer(){
 	return buffer;
 }
+*/
+//I guess im not gonna include the getBuffer since its already in Wav
 
 void CSV::writeFile(const std::string &outFileName){
 	//do I need line below?
@@ -39,10 +41,30 @@ void CSV::writeFile(const std::string &outFileName){
 	//outFile.close();
 }
 
+//is this better than the one above?
+void CSV::writeFile(const std::string &outFileName){
+	if(file.is_open()){
+		outFile.write((char*)&waveHeader, Wav.title);
+		outFile.write((char*)&waveHeader, Wav.artist);
+	}else{
+		std::cout << "Cannot open CSV file";
+		outFile.close();
+	}
+}
+
 CSV::~CSV(){
 	if(buffer != NULL){
 		delete[] buffer;
 	}
 }
 
-//void CSV::setData();
+//Im not sure if we need the bottom stuff - Annie
+/*
+std::string CSV::getTitle() const{
+	return title;
+}
+
+std::string CSV::getArtist() const{
+	return artist;
+}
+*/
--- csv.h
+++ csv.h
@@ -1,19 +1,24 @@
 #ifndef CSV_H
 #define CSV_H
 
-//#include "GroupProj_CS202 - Sheet1.csv"
+#include "GroupProj_CS202 - Sheet1.csv"
 #include "wav.h"
 
-class CSV{
-	unsigned char* buffer = NULL;
-	std::string data;
+class CSV : public Wav{
+	//unsigned char* buffer = NULL;
+	std::string title;
+	std::string artist;
 public:
-	CSV(std::string data);
+	Wav.title;
+	Wav.artist;
+	
+	CSV(std::string title, std::string artist);
 	void writeFile(const std::string &outFileName);
 
-	unsigned char* getBuffer();
-	int getBuffersize() const;
-	std::string getData() const;
+	//unsigned char* getBuffer();
+	//int getBuffersize() const;
+	std::string getTitle() const;
+	std::string getArtist() const;
 	void setData() const;
 
 	virtual ~CSV();
